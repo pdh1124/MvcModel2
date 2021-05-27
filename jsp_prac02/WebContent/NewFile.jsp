9a1246d89de047e833321d27d5cd792b8f5b20d8
pdh1124 <pdw3231@naver.com>
2021-05-27 23:47:59 +0900

    서블렛을 통해 요청과 응답 테스트

--- jsp_prac02/WebContent/NewFile.jsp
+++ jsp_prac02/WebContent/NewFile.jsp
@@ -1,12 +1,35 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> <!-- page 지시어. -->
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<style>
+#loginFormArea {
+	text-align: center;
+	width: 280px;
+	margin: auto;
+	border: 1px solid red;
+}
+
+h1 {
+	text-align: center;	
+}
+</style>
 </head>
 <body>
-
+	<h1>로그인</h1>
+	<section id="loginFormArea">
+		<form action="login" method="post"> 
+		<!-- method="get, post" 방식 2가지가 있는데 get은 주소창에 넘긴 주소가 표시되고, post방식은 주소에 표시가 되지 않음-->
+			<!-- action 속성은 폼의 입력결과를 처리할 페이지를 지정. method는 전송 방식, post 방식도 있음 -->
+			<label id="id">아이디 :</label>
+			<input type="text" name="id" id="id" /><br>
+			<label id="password">비밀번호 :</label>
+			<input type="password" name="password" id="password" /><br><br>
+			<input type="submit" value="로그인" />
+		</form>
+	</section>
 </body>
 </html>
---
+++ jsp_prac02/src/LoginServlet.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//요청에 대한 처리
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		//응답 화면 만들기
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("아이디=" + id + "<br>");
+		out.println("비밀번호=" + password + "<br>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
