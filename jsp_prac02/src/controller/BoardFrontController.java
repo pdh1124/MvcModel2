c6b0bea4b10d0e8f60900c64b6a1700e64c9fc7c
pdh1124 <pdw3231@naver.com>
2021-05-29 16:49:29 +0900

    글쓰기 처리 중

---
+++ jsp_prac02/src/controller/BoardFrontController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.ActionForward;
+
+
+@WebServlet("*.bo")
+public class BoardFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public BoardFrontController() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGet(request, response);
+		doProcess(request, response);
+	}
+
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		
+		ActionForward forward = null;
+		
+	}
+	
+	
+
+}
---
+++ jsp_prac02/src/db/JdbcUtil.java
@@ -0,0 +1,61 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class JdbcUtil {
+	
+	//디비 연결처리
+	public static Connection getConnection() throws Exception {
+		
+		Connection conn = null;
+		Context init = new InitialContext();
+		DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+		
+		conn = ds.getConnection();
+		
+		return conn;
+	}
+	
+	//데이터 베이스는 외부 자원으로 사용후, 자원 반납
+	public static void close(Connection con) {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Statement 실행 후 반납
+	public static void close(Statement stmt) {
+		try {
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//쿼리문 실행 완료(커밋)
+	public static void commit(Connection con) {
+		try {
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//전달된 쿼리문의 실행 취소(롤백)
+	public static void rollback(Connection con) {
+		try {
+			con.rollback();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
